6f9dccd682977d3d2d7f8fcab4c6ecce7a3ac31f
Zsolt <kzsolt3141@gmail.com>
2022-01-14 12:26:38 +0200

    Update submodules (simplifi API)

--- test/main.c
+++ test/main.c
@@ -64,8 +64,6 @@ static void TIMER2_PWM_cb_handle(void* ctx) {
 }
 
 int main(void) {
-    uint8_t sts = 0;
-
     // UART INIT
     //-------------------------------
     const uint16_t baud_rate = 38400;
@@ -74,8 +72,7 @@ int main(void) {
 
     regiter_USART_RXC_cb(USART_RXC_cb_handle, &USART_RXC_ctx);
 
-    sts = USART_init(baud_rate, 1);
-    if (sts) return sts;
+    USART_init(baud_rate);
 
     printf("Init Done UART baud: %u\n", (uint16_t)baud_rate);
     //-------------------------------
@@ -86,8 +83,7 @@ int main(void) {
 
     regiter_ADC_isr_cb(ADC_cb_handle, &ctx);
 
-    sts = ADC_init(ADC_PS_128, 1, 1);
-    if (sts) return sts;
+    ADC_init(ADC_PS_128, 1);
 
     DDRB &= ~((1 << PC0) || (1 << PC1));  //set input for ADC
 
@@ -97,8 +93,7 @@ int main(void) {
     //-------------------------------
     regiter_TIMER2_isr_cb(TIMER2_PWM_cb_handle, &ctx);
 
-    sts = TIMER2_PWM_init(0, TIMER2_PS_PRESCALE_256, 1);
-    if (sts) return sts;
+    TIMER2_PWM_init(0, TIMER2_PS_PRESCALE_256);
 
     printf("Init Done TIMER2\n");
     //-------------------------------
